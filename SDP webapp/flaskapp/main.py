96ea9e8c9c1c22a7feaec59831da77b7bb5be275
Zihao Cai <40333535+IrwinChay@users.noreply.github.com>
2022-04-12 14:26:38 +0100

    update directory

--- SDP webapp/flaskapp/main.py
+++ SDP webapp/flaskapp/main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 import ssh
+import client
+import json
+import time
+from timeloop import Timeloop
+from datetime import timedelta
 from flask import Flask,Blueprint,render_template, url_for, request, redirect, flash
 from flask_login import login_user,logout_user,login_required
 from werkzeug.security import check_password_hash
@@ -15,6 +20,12 @@ def home():
 def contact():
     return render_template("contact.html")
 
+@main.route('/sensors')
+def sensor_reading():
+    if (raspPi):
+        return jsonify(grove.sensor_readings())
+    return ""
+
 @main.route("/settings", methods=['POST', 'GET'])
 def settings():
     if request.method == "POST":
@@ -29,10 +40,49 @@ def settings():
         ssh.run_auto_mode((Hour_value * 3600 + Minute_value * 60 + Sec_value), Auto_mode_value) # Make sure runtime is in seconds
     return render_template("settings.html")         
 
+@main.route('/admin')
+def admin():
+    with open('plots.json', 'r', encoding="utf8") as f:
+        content = f.read()
+    content = content.replace('\n', ' ').replace('\r', '')
+    with open('db.json', 'r', encoding="utf8") as f:
+        content2 = f.read()
+    content2 = content2.replace('\n', ' ').replace('\r', '')
+    return render_template('admin.html', plotsJson = content, dbJSON = content2)
+
+@main.route('/overrides')
+def overrides():
+    return render_template('overrides.html', sensor_reading="Click to view current sensor readings")
+
+@main.route('/move')
+def move():
+    if (request.args.get('direction', default = 'none', type = str) == 'forward'):
+        message = "f"
+    elif (request.args.get('direction', default = 'forward', type = str) == 'backward'):
+        message="b"
+    else:
+        message = request.args.get('x', default = '1', type= str) +','+ request.args.get('y', default = '1', type = str)
+    print("Message is: " + message)
+    client.initClient(message)
+
+    # add to action log
+    action = 'Moved to position ' + message
+    addToActions(action)
+
+    return render_template('overrides.html', sensor_reading="Click to view current sensor readings")
+
 @main.route("/head")
 def head():
     return render_template("head.html")
 
+@main.route('/gridReact')
+def gridding():
+    with open('plots.json', 'r', encoding="utf8") as f:
+        content = f.read()
+    content = content.replace('\n', ' ').replace('\r', '')
+    return render_template('grid11.html', plotsJson = content)
 
-#if __name__ == "__main__":
-    #app.run(debug=True)
+@main.route("/getPlots")
+def getPlots():
+    return "plots.json"
+
